9f977e3b50461162f7f9f5b2229a68ca853fe165
levi106 <levi106@gmail.com>
2021-09-24 16:59:10 +0900

    print request header

--- backapi/src/main/java/com/example/backapi/controller/ReactorController.java
+++ backapi/src/main/java/com/example/backapi/controller/ReactorController.java
@@ -1,8 +1,11 @@
 package com.example.backapi.controller;
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +17,8 @@ public class ReactorController {
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping("/{id}")
-    public Mono<String> get(@PathVariable Integer id) {
+    public Mono<String> get(@PathVariable Integer id, @RequestHeader Map<String, String> headers) {
+        headers.forEach((k, v) -> LOGGER.info("{'name':'{}', 'value':'{}'}", k, v));
         return Mono.just(id)
             .map(x -> {
                 LOGGER.info("Thread # {}: map", Thread.currentThread().getId());
